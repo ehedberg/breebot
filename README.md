4632ee66d90e4bca0e22368ded631c1bafdbc06d
Eric Hedberg <ericjhedberg@gmail.com>
2015-11-09 14:20:15 -0600

    add a read

---
+++ README.md
@@ -0,0 +1,8 @@
+# BreeBot.py
+
+How dare Bree leave the team? I'm sure we'll all miss her, which is why I've decided to replace her with a small shell script.
+
+# Prerequisites
+
+1. `pip install praw`.
+2. add a praw.ini with user info. look it up, it's not that hard.
--- breebot.py
+++ breebot.py
@@ -13,7 +13,6 @@ reddit.login()
 words = ['smoke', 'fire', 'pyro', 'flare', 'flares']
 
 while True:
-	print "running"
 	subreddit = reddit.get_subreddit('minnesotaunited')
 
 	for comment in subreddit.get_comments():
@@ -21,6 +20,7 @@ while True:
 		if interesting and db.has_key(comment.id) == False:
 			new_comment = '*N%s%s*' % ('o' * int(random.uniform(8,20)), '!' * int(random.uniform(1,4)))
 			comment.reply(new_comment)
-			print comment.body
 			db[comment.id] = 'T'
-	time.sleep(60)
+			print time.asctime()
+
+	time.sleep(300)
